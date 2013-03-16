d4da86387144fc56525f51acd27c2980946a5e65
ytrusty <black.caffeine@gmail.com>
2013-03-16 14:24:37 +0800

    Initial Commit

---
+++ newKeyDiary/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  newKeyDiary
+//
+//  Created by Black Black on 3/16/13.
+//  Copyright (c) 2013 BlackXBlack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ newKeyDiary/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  newKeyDiary
+//
+//  Created by Black Black on 3/16/13.
+//  Copyright (c) 2013 BlackXBlack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
